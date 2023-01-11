7ae3471814d0ae170d1d572d91483f2b3b0355b0
Kirill Guryanov <kg.workspaces@gmail.com>
2023-01-11 18:51:34 +0300

    change intro section

--- source/js/index.js
+++ source/js/index.js
@@ -2,11 +2,14 @@
 import "./header/header"
 import "./header/burger"
 // intro
+import "./intro/splide"
 import "./intro/swiper"
-import "./intro/swiper-pagination-to-left-side"
+// import "./intro/swiper-pagination-to-left-side"
 // about us
 import "./about-us/decoration-image"
 import "./about-us/running-line"
+//services
+import "./services/lines"
 // footer
 import "./footer/rights-top-border"
 import "./footer/checkbox"
--- source/js/intro/swiper.js
+++ source/js/intro/swiper.js
@@ -1,33 +1,36 @@
-const swiper = new Swiper(".intro__slider-swiper", {
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-
-  loop: true,
-  slidesPerView: 3,
-  spaceBetween: 0,
-  mousewheel: true,
-
-  breakpoints: {
-    // 1380 >= 1380
-    1381: {
-      direction: "vertical",
-    },
-    // when window width is >= 480px
-    800: {
-      direction: "horizontal",
-      slidesPerView: 3,
-      spaceBetween: 10,
-    },
-    500: {
-      slidesPerView: 2,
-      spaceBetween: 10,
-    },
-    0: {
-      slidesPerView: 1,
-    }
-  }
-});
-
-console.log("Haha")
+// const swiper = new Swiper(".swiper", {
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//
+//   draggable: true,
+//   loop: true,
+//   slidesPerView: 1,
+//   spaceBetween: 0,
+//   mousewheel: true,
+//   direction: "horizontal",
+//   centeredSlides: true
+//
+//   // breakpoints: {
+//   //   // 1380 >= 1380
+//   //   1381: {
+//   //     direction: "vertical",
+//   //   },
+//   //   // when window width is >= 480px
+//   //   800: {
+//   //     direction: "horizontal",
+//   //     slidesPerView: 3,
+//   //     spaceBetween: 10,
+//   //   },
+//   //   500: {
+//   //     slidesPerView: 2,
+//   //     spaceBetween: 10,
+//   //   },
+//   //   0: {
+//   //     slidesPerView: 1,
+//   //   }
+//   // }
+// });
+//
+// console.log("Haha")
