6cd97e1a34218dec43179b79e047adc8722b16fe
admin <kg.workspaces@gmail.com>
2023-03-10 16:20:21 +0300

    add modals

--- source/js/modals/modals.js
+++ source/js/modals/modals.js
@@ -1,81 +1,78 @@
 document.addEventListener("DOMContentLoaded", () => {
   const modalTriggers = document.querySelectorAll(".modalTrigger");
   const wrappers = document.querySelectorAll(".wrapper");
-  const modals = document.querySelectorAll(".modal");
+  //   const modals = document.querySelectorAll(".modal");
 
   modalTriggers.forEach((modalTrigger) => {
     modalTrigger.addEventListener("click", () => {
-      modals.forEach((modal) => {
-        wrappers.forEach((wrapper) => {
-          const isModalTrigger = modalTrigger.dataset.modal;
-          const isModal = modal.dataset.modal;
-          const isWrapper = wrapper.dataset.modal;
+      //   modals.forEach((modal) => {
+      wrappers.forEach((wrapper) => {
+        const isModalTrigger = modalTrigger.dataset.modal;
+        // const isModal = modal.dataset.modal;
+        const isWrapper = wrapper.dataset.modal;
 
-          if (
-            isModalTrigger &&
-            isWrapper &&
-            isModalTrigger === isWrapper &&
-            isModalTrigger === isModal
-          ) {
-            wrapper.classList.add("wrapper--visible");
-            modal.classList.add("modal--visible");
+        if (isModalTrigger && isWrapper && isModalTrigger === isWrapper) {
+          wrapper.classList.add("wrapper--visible");
+          //   modal.classList.add("modal--visible");
 
-            const body = document.querySelector("body");
-            body.classList.add("hidden");
-          }
-        });
+          const body = document.querySelector("body");
+          body.classList.add("hidden");
+        }
       });
     });
+    // });
   });
 });
 
 document.addEventListener("DOMContentLoaded", () => {
   const wrappers = document.querySelectorAll(".wrapper");
-  const modals = document.querySelectorAll(".modal");
+  //   const modals = document.querySelectorAll(".modal");
 
   wrappers.forEach((wrapper) => {
-    wrapper.addEventListener("click", () => {
-      wrappers.forEach((wrapper) => {
-        modals.forEach((modal) => {
+    wrapper.addEventListener("click", (e) => {
+      if (e.target === wrapper) {
+        wrappers.forEach((wrapper) => {
+          //   modals.forEach((modal) => {
           wrapper.classList.remove("wrapper--visible");
-          modal.classList.remove("modal--visible");
+          // modal.classList.remove("modal--visible");
 
           const body = document.querySelector("body");
           body.classList.remove("hidden");
         });
-      });
+        // });
+      }
     });
 
-    modals.forEach((modal) => {
-      const closeButton = modal.querySelector(".modal__contentCross");
+    // modals.forEach((modal) => {
+    const closeButton = wrapper.querySelector(".modal__contentCross");
 
-      closeButton.addEventListener("click", () => {
-        wrapper.classList.remove("wrapper--visible");
-        modal.classList.remove("modal--visible");
+    closeButton.addEventListener("click", () => {
+      wrapper.classList.remove("wrapper--visible");
+      // modal.classList.remove("modal--visible");
 
-        const body = document.querySelector("body");
-        body.classList.remove("hidden");
-      });
+      const body = document.querySelector("body");
+      body.classList.remove("hidden");
     });
+    // });
   });
 });
 
 document.addEventListener("keyup", (e) => {
   const wrappers = document.querySelectorAll(".wrapper");
-  const modals = document.querySelectorAll(".modal");
+  //   const modals = document.querySelectorAll(".modal");
 
   wrappers.forEach((wrapper) => {
-    modals.forEach((modal) => {
-      const visibleWrapper = wrapper.classList.contains("wrapper--visible");
-      const visibleModal = modal.classList.contains("modal--visible");
+    // modals.forEach((modal) => {
+    const visibleWrapper = wrapper.classList.contains("wrapper--visible");
+    //   const visibleModal = modal.classList.contains("modal--visible");
 
-      if (e.key === "Escape" && visibleWrapper && visibleModal) {
-        wrapper.classList.remove("wrapper--visible");
-        modal.classList.remove("modal--visible");
+    if (e.key === "Escape" && visibleWrapper) {
+      wrapper.classList.remove("wrapper--visible");
+      // modal.classList.remove("modal--visible");
 
-        const body = document.querySelector("body");
-        body.classList.remove("hidden");
-      }
-    });
+      const body = document.querySelector("body");
+      body.classList.remove("hidden");
+    }
   });
+  //   });
 });
